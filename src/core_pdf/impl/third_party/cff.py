aad68a00334dc011c5a9c4ff1f0766c2ca731856
Kevin Turcios <turcioskevinr@gmail.com>
2026-07-16 22:38:34 -0500

    fix(fonts): map all standard CFF glyph names

--- src/core_pdf/impl/third_party/cff.py
+++ src/core_pdf/impl/third_party/cff.py
@@ -69,18 +69,47 @@ STANDARD_GLYPH_SIDS = {
         )
     )
 }
-STANDARD_GLYPH_SIDS.update(
-    {
-        "quotesingle": 104,
-        "quotedblleft": 105,
-        "endash": 111,
-        "bullet": 116,
-        "quotedblright": 119,
-        "emdash": 137,
-        "multiply": 168,
-    }
-)
 CFF_STANDARD_STRING_COUNT = 391
+_STANDARD_GLYPH_NAMES_AFTER_ASCII = """
+exclamdown cent sterling fraction yen florin section currency quotesingle quotedblleft
+guillemotleft guilsinglleft guilsinglright fi fl endash dagger daggerdbl periodcentered
+paragraph bullet quotesinglbase quotedblbase quotedblright guillemotright ellipsis perthousand
+questiondown grave acute circumflex tilde macron breve dotaccent dieresis ring cedilla
+hungarumlaut ogonek caron emdash AE ordfeminine Lslash Oslash OE ordmasculine ae dotlessi
+lslash oslash oe germandbls onesuperior logicalnot mu trademark Eth onehalf plusminus Thorn
+onequarter divide brokenbar degree thorn threequarters twosuperior registered minus eth
+multiply threesuperior copyright Aacute Acircumflex Adieresis Agrave Aring Atilde Ccedilla
+Eacute Ecircumflex Edieresis Egrave Iacute Icircumflex Idieresis Igrave Ntilde Oacute
+Ocircumflex Odieresis Ograve Otilde Scaron Uacute Ucircumflex Udieresis Ugrave Yacute
+Ydieresis Zcaron aacute acircumflex adieresis agrave aring atilde ccedilla eacute
+ecircumflex edieresis egrave iacute icircumflex idieresis igrave ntilde oacute ocircumflex
+odieresis ograve otilde scaron uacute ucircumflex udieresis ugrave yacute ydieresis zcaron
+exclamsmall Hungarumlautsmall dollaroldstyle dollarsuperior ampersandsmall Acutesmall
+parenleftsuperior parenrightsuperior twodotenleader onedotenleader zerooldstyle oneoldstyle
+twooldstyle threeoldstyle fouroldstyle fiveoldstyle sixoldstyle sevenoldstyle eightoldstyle
+nineoldstyle commasuperior threequartersemdash periodsuperior questionsmall asuperior bsuperior
+centsuperior dsuperior esuperior isuperior lsuperior msuperior nsuperior osuperior rsuperior
+ssuperior tsuperior ff ffi ffl parenleftinferior parenrightinferior Circumflexsmall
+hyphensuperior Gravesmall Asmall Bsmall Csmall Dsmall Esmall Fsmall Gsmall Hsmall Ismall Jsmall
+Ksmall Lsmall Msmall Nsmall Osmall Psmall Qsmall Rsmall Ssmall Tsmall Usmall Vsmall Wsmall
+Xsmall Ysmall Zsmall colonmonetary onefitted rupiah Tildesmall exclamdownsmall centoldstyle
+Lslashsmall Scaronsmall Zcaronsmall Dieresissmall Brevesmall Caronsmall Dotaccentsmall
+Macronsmall figuredash hypheninferior Ogoneksmall Ringsmall Cedillasmall questiondownsmall
+oneeighth threeeighths fiveeighths seveneighths onethird twothirds zerosuperior foursuperior
+fivesuperior sixsuperior sevensuperior eightsuperior ninesuperior zeroinferior oneinferior
+twoinferior threeinferior fourinferior fiveinferior sixinferior seveninferior eightinferior
+nineinferior centinferior dollarinferior periodinferior commainferior Agravesmall Aacutesmall
+Acircumflexsmall Atildesmall Adieresissmall Aringsmall AEsmall Ccedillasmall Egravesmall
+Eacutesmall Ecircumflexsmall Edieresissmall Igravesmall Iacutesmall Icircumflexsmall
+Idieresissmall Ethsmall Ntildesmall Ogravesmall Oacutesmall Ocircumflexsmall Otildesmall
+Odieresissmall OEsmall Oslashsmall Ugravesmall Uacutesmall Ucircumflexsmall Udieresissmall
+Yacutesmall Thornsmall Ydieresissmall 001.000 001.001 001.002 001.003 Black Bold Book Light
+Medium Regular Roman Semibold
+""".split()  # noqa: SIM905 - compact ordered copy of the CFF specification table
+STANDARD_GLYPH_SIDS.update(
+    {name: sid for sid, name in enumerate(_STANDARD_GLYPH_NAMES_AFTER_ASCII, start=96)}
+)
+assert len(STANDARD_GLYPH_SIDS) == CFF_STANDARD_STRING_COUNT
 
 
 class CFFFont:
