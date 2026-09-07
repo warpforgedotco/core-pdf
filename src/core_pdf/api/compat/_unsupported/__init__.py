86e215c8a44839d15073053b286a18449563c72c
Kevin Turcios <turcioskevinr@gmail.com>
2026-09-06 22:58:44 -0500

    dot

---
+++ src/core_pdf/api/compat/_unsupported/__init__.py
@@ -0,0 +1,3 @@
+"""this module is where we put all the unsupported compatibility libraries.
+This is to avoid cluttering the main compatibility
+module with unsupported libraries that may not be maintained or may not work as expected."""
